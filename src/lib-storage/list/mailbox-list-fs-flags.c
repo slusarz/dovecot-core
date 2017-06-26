bd03357d048930d2fbb4afeb6410c4097f047971
Timo Sirainen <timo.sirainen@dovecot.fi>
2017-06-26 20:10:17 +0300

    lib-storage: Add mail_location=..:ITERINDEX
    This changes mailbox list iteration to work using INDEX directory instead of
    the normal mail directory. This can be helpful when the indexes are stored
    on a faster storage.

--- src/lib-storage/list/mailbox-list-fs-flags.c
+++ src/lib-storage/list/mailbox-list-fs-flags.c
@@ -118,12 +118,13 @@ int fs_list_get_mailbox_flags(struct mailbox_list *list,
 
 	*flags_r = 0;
 
-	if (*list->set.maildir_name != '\0') {
+	if (*list->set.maildir_name != '\0' && !list->set.iter_from_index_dir) {
 		/* maildir_name is set: the code is common for all
 		   storage types */
 		return list_is_maildir_mailbox(list, dir, fname, type, flags_r);
 	}
-	if (list->v.is_internal_name != NULL &&
+	if (!list->set.iter_from_index_dir &&
+	    list->v.is_internal_name != NULL &&
 	    list->v.is_internal_name(list, fname)) {
 		/* skip internal dirs */
 		*flags_r |= MAILBOX_NOSELECT;
@@ -195,7 +196,7 @@ int fs_list_get_mailbox_flags(struct mailbox_list *list,
 
 	if ((list->flags & MAILBOX_LIST_FLAG_MAILBOX_FILES) != 0) {
 		*flags_r |= STAT_GET_MARKED_FILE(st);
-	} else if (list->v.is_internal_name == NULL) {
+	} else if (list->v.is_internal_name == NULL || list->set.iter_from_index_dir) {
 		/* link count < 2 can happen with filesystems that don't
 		   support link counts. we'll just ignore them for now.. */
 		if (st.st_nlink == 2)
